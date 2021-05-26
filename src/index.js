c65e6c55d40039e6ab9318efa2b3d1d12d1a3628
BondarenkoAlla <bondarenkoalla961@gmail.com>
2021-05-26 18:58:37 +0300

    HW12

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+import './sass/main.scss';
+import fetchCountries from './fetchCountries';
+import countryHbs from './templates/country.hbs';
+import listHbs from './templates/list.hbs';
+import debounce from 'lodash.debounce';
+const refs = {
+  searchInput: document.querySelector('.input'),
+  cotainer: document.querySelector('.container-js'),
+};
+
+import {error} from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css'
+import '@pnotify/core/dist/BrightTheme.css'
+function toManyMatches() {
+  error({
+    text: 'To many matches found. Please enter a more specific query!',
+    delay: 1500,
+  })
+};
+function countryNotFound() {
+  error({
+    text: 'Country not found!',
+    delay: 1500,
+  });
+}
+refs.searchInput.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+  const searchQuery = e.target.value;
+  fetchCountries(searchQuery)
+    .then(countryShow);    ;
+}
+
+
+const countryShow = countries => {
+  if (countries.length > 10) { toManyMatches() };
+  if (countries.status === 404) { countryNotFound() };
+  if (countries.length > 1 && countries.length < 10) { refs.cotainer.innerHTML = listHbs(countries) };
+  if (countries.length === 1) { refs.cotainer.innerHTML = countryHbs(countries) };
+
+}
+
